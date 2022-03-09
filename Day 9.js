367be59121e2259e226da4e2274740092b99b5d5
AjibolaYusuf <ajibolayusuf8@gmail.com>
2022-03-09 11:07:56 +0100

    project init

--- Day 9.js
+++ Day 9.js
@@ -1,3 +1,4 @@
+// Exercises on Day-9 in 30-DAYS OF JAVASCRIPT
 const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -13,3 +14,8 @@ const products = [
 const callback = function(item){console.log(item)}
 let countriesNames = countries.forEach(callback)
 console.log(countriesNames)
+console.log(numbers.forEach(callback))
+console.log(names.forEach(callback))
+const newNames = names.map(item => item.toUpperCase())
+console.log(newNames)
+
